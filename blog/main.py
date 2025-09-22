f674d9bd5405c318ee7ffe697d4bdd605b1e148f
Dabeey <abbasdaughter18@gmail.com>
2025-09-22 14:51:18 +0100

    feat(user): implement user creation and retrieval endpoints

--- blog/main.py
+++ blog/main.py
@@ -1,44 +1,10 @@
-from fastapi import FastAPI, Depends, status, Response, HTTPException
-from .schemas import BlogSchema, ShowBlog, UserSchema, ShowUser
-from .models import Blog, User
-from .database import Base,engine, get_db
-from sqlalchemy.orm import Session
-from passlib.context import CryptContext
-from .hashing import Hash
-from typing import List
-from .routers import blog
-
+from fastapi import FastAPI
+from .database import Base,engine
+from .routers import blog, user
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
 app.include_router(blog.router)
-
-
-# @app.post('/blog',status_code=status.HTTP_201_CREATED, response_model = ShowBlog, tags=['blogs'] )
-# def create(request:BlogSchema, db: Session = Depends(get_db) ):
-#     new_blog = Blog(title = request.title, body = request.body, user_id = 1)
-#     db.add(new_blog)
-#     db.commit()
-#     db.refresh(new_blog)
-#     return new_blog
-
-
-@app.post('/user', response_model=ShowUser, tags=['user'])
-def create_user(request:UserSchema, db: Session = Depends(get_db)):
-    new_user = User(name = request.name, email = request.email, password = Hash.bcrypt(request.password))
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
-
-
-
-@app.get('/users/{id}', status_code=200, response_model=ShowUser, tags=['user'])
-def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.id==id).first()
-
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with id {id} not found.')
-    return user
+app.include_router(user.router)
---
+++ blog/routers/user.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, FastAPI, Depends, status, HTTPException
+from ..schemas import UserSchema, ShowUser
+from ..models import User
+from ..database import SessionLocal, get_db
+from sqlalchemy.orm import Session
+from typing import List
+from ..hashing import Hash
+
+
+router = APIRouter()
+app = FastAPI()
+
+
+
+@router.post('/user', response_model=ShowUser, tags=['user'])
+def create_user(request:UserSchema, db: Session = Depends(get_db)):
+    new_user = User(name = request.name, email = request.email, password = Hash.bcrypt(request.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+
+@router.get('/users/{id}', status_code=200, response_model=ShowUser, tags=['user'])
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id==id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with id {id} not found.')
+    return user
