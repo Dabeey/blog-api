48ecd55b309360098702462e636a11635d6172f4
Dabeey <abbasdaughter18@gmail.com>
2025-09-23 13:18:28 +0100

    feat: implement latest changes and improvements to blog API

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -16,25 +16,25 @@ app = FastAPI()
 
 
 @router.get('', response_model=List[ShowBlog])
-def all(db: Session = Depends(get_db), get_current_user: UserSchema = Depends(get_current_user) ):
+def all(db: Session = Depends(get_db), current_user: UserSchema = Depends(get_current_user) ):
     return blog.get_all(db)
 
 
 @router.post('',status_code=status.HTTP_201_CREATED, response_model = ShowBlog )
-def create(request:BlogSchema, db: Session = Depends(get_db) ):
+def create(request:BlogSchema, db: Session = Depends(get_db), current_user: UserSchema = Depends(get_current_user) ):
     return blog.create_blog(request, db)
 
 
 @router.get('/{id}', status_code=200, response_model = ShowBlog)
-def show(id: int, db: Session = Depends(get_db)):
+def show(id: int, db: Session = Depends(get_db), current_user: UserSchema = Depends(get_current_user)):
     return blog.show_blog(id, db)
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id: int, db: Session = Depends(get_db)):
+def destroy(id: int, db: Session = Depends(get_db), current_user: UserSchema = Depends(get_current_user)):
     return blog.delete_blog(id, db)
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED,response_model = ShowBlog)
-def update(id: int, request: BlogSchema, db: Session = Depends(get_db) ):
+def update(id: int, request: BlogSchema, db: Session = Depends(get_db), current_user: UserSchema = Depends(get_current_user) ):
     return blog.update_blog(id, request, db)
